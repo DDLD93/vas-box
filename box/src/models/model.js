c0d17e2e2062eb65a030f73596cf92e01b75dcea
ddld93 <umar.jere@gmail.com>
2023-06-08 16:36:03 +0100

    commit

---
+++ box/src/models/model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MeessageSchema = new Schema({
+    title: { type: String, index: { unique: true, } },
+    senderId: { type: String },
+    recepients: {
+        type: [{
+            phone: { type: String },
+            network: { type: String },
+            isDelievered: { type: Boolean },
+
+        }],
+    },
+    summary: {
+        type: {
+            total: { type: Number },
+            invalid: { type: Number },
+            MTN: { type: Number },
+            GLO: { type: Number },
+            AIRTEL: { type: Number },
+            ETISALAT: { type: Number },
+        }
+    },
+    status: { type: String, enum: ["Pending", "Suspended", "Sent"], default: "Pending" },
+    updatedAt: { type: Date, default: Date.now() },
+    createdAt: { type: Date, default: Date.now() },
+});
+
+
+module.exports = mongoose.model("Meessage", MeessageSchema);
